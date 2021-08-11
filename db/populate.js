41ab07f7304e2821af5f8ca887d58f4b86bc81fa
Cam Boucher <cam.boucher.28@gmail.com>
2021-08-11 14:07:27 -0400

    Finished ETL process

--- db/populate.js
+++ db/populate.js
@@ -5,25 +5,25 @@ var db = require('../db');
 const createTables = () => {
   return db.query(
   `CREATE TABLE reviews(
-    id integer PRIMARY KEY,
-    product integer,
-    rating integer,
-    summary varchar(60),
-    recommended boolean,
-    name varchar,
-    helpfulness integer,
-    reported boolean,
-    email varchar,
-    body varchar(1000),
-    response varchar,
-    date timestamp
+    id INTEGER PRIMARY KEY,
+    product INTEGER,
+    rating INTEGER,
+    date VARCHAR,
+    summary VARCHAR,
+    body VARCHAR(1000),
+    recommend BOOLEAN,
+    reported BOOLEAN,
+    name VARCHAR,
+    email VARCHAR,
+    response VARCHAR,
+    helpfulness INTEGER
   );`)
   .then((res) => {
     return db.query(
     `CREATE TABLE photos(
-      id integer PRIMARY KEY,
-      url varchar,
-      review_id integer,
+      id INTEGER PRIMARY KEY,
+      review_id INTEGER,
+      url VARCHAR,
         CONSTRAINT fk_review
         FOREIGN KEY(review_id) REFERENCES reviews(id)
   );`)})
@@ -31,15 +31,47 @@ const createTables = () => {
     return db.query(
     `CREATE TABLE characteristics (
       id integer PRIMARY KEY,
-      name varchar,
-      value integer,
-      review_id integer,
+      characteristic_id INTEGER,
+      review_id INTEGER,
+      value INTEGER,
+      name VARCHAR,
       CONSTRAINT fk_review
         FOREIGN KEY(review_id) REFERENCES reviews(id)
     );`
   )})
+  .then((res) => {
+    return db.query(
+      `CREATE TABLE raw_chars (
+        id INTEGER PRIMARY KEY,
+        product INTEGER,
+        name VARCHAR
+      );`
+    )
+  })
   .then((res) => console.log(res))
   .catch(err => console.log(err))
 };
 
 createTables();
+
+
+/*
+===================
+ETL copy statements
+===================
+
+psql -d chunky-reviews -U camboucher -c "\copy reviews(id, product, rating, date, summary, body, recommend, reported, name, email, response, helpfulness) FROM reviews.csv' DELIMITER ',' CSV HEADER;"
+
+psql -d chunky-reviews -U camboucher -c "\copy photos(id, review_id, url) FROM 'raw_chars.csv' DELIMITER ',' CSV HEADER;"
+
+psql -d chunky-reviews -U camboucher -c "\copy raw_chars(id, product, name) FROM 'raw_chars.csv' DELIMITER ',' CSV HEADER;"
+
+psql -d chunky-reviews -U camboucher -c "\copy characteristics(id, characteristic_id, review_id, value) FROM 'characteristics.csv' DELIMITER ',' CSV HEADER;"
+
+//this last statement adds the characteristic name to the characteristics table from the raw_chars table
+
+UPDATE characteristics SET name = (SELECT name FROM raw_chars WHERE characteristics.characteristic_id = raw_chars.id);
+
+*/
+
+
